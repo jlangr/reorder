252f887162bb95c115d1912e8d18804c665ed34a
Jeff Langr <jeff@langrsoft.com>
2016-04-04 14:34:12 -0400

    support java 1.7 empty list

--- test/util/RangeTest.java
+++ test/util/RangeTest.java
@@ -2,13 +2,14 @@ package util;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static java.util.Collections.EMPTY_LIST;
 import org.junit.Test;
 import static java.util.Arrays.asList;
 
 public class RangeTest {
    @Test
    public void removeFromListMatchingSizeReturnsEmptyList() {
-      assertThat(new Range(0, 1).removeFrom(asList(10, 20)), equalTo(asList()));
+      assertThat(new Range(0, 1).removeFrom(asList(10, 20)), equalTo(EMPTY_LIST));
    }
 
    @Test
